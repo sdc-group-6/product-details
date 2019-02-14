5878f98baac37976738fd8e9061d7793c8c0013b
Kelly Hudiono <kellyhudiono@gmail.com>
2019-02-14 09:58:07 -0800

    write unit tests for App

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -7,22 +7,55 @@ import Likes from '../client/src/components/Likes';
 import Looks from '../client/src/components/Looks';
 import Share from '../client/src/components/Share';
 import Topbar from '../client/src/components/Topbar';
+import Specification from '../client/src/components/Specification';
 
 describe('App Component', () => {
   const component = shallow(<App />);
   it('should render Description', () => {
     expect(component.find(Description)).toHaveLength(1);
   });
+
   it('should render Likes', () => {
     expect(component.find(Likes)).toHaveLength(1);
   });
+
   it('should render Looks', () => {
     expect(component.find(Looks)).toHaveLength(1);
   });
+
   it('should render Share', () => {
     expect(component.find(Share)).toHaveLength(1);
   });
+
   it('should render Topbar', () => {
     expect(component.find(Topbar)).toHaveLength(1);
   });
+
+  it('should not render Specification on page load', () => {
+    expect(component.find(Specification)).toHaveLength(0);
+  });
+
+  it('should show Specification on click', () => {
+    const clickFn = jest.fn();
+    const component = shallow(<App specClick={clickFn} />);
+
+    component.find('div#spec').simulate('click')
+    
+    expect(component.find('div#spec')).toHaveLength(1);
+    expect(component.find(Specification)).toHaveLength(1);
+    expect(component.find(Description)).toHaveLength(0);
+  })
+
+  it('should be able to toggle from Specification and Description', () => {
+    const clickFn = jest.fn();
+    const component = shallow(<App onClick={clickFn} />);
+
+    component.find('div#spec').simulate('click');
+    expect(component.find(Specification)).toHaveLength(1);
+    expect(component.find(Description)).toHaveLength(0);
+
+    component.find('div#desc').simulate('click');
+    expect(component.find(Specification)).toHaveLength(0);
+    expect(component.find(Description)).toHaveLength(1);
+  })
 })

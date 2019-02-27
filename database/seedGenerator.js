056e2cb675fa743f170ca8d3d50cc7c6838c3763
ronnywoods <rawoods93@gmail.com>
2019-02-27 11:34:33 -0800

    Add empty seedGenerator

---
+++ database/seedGenerator.js
@@ -0,0 +1,11 @@
+module.exports = (recordCount, sql) => {
+  let products = {};
+  let looks = {};
+  let shares = {};
+  
+  // random number helper
+  
+  // determine number of each type of product to generate
+
+  return { products, looks, shares };
+};

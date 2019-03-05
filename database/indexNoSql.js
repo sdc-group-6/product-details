84b9afefb1b33e0bbb051f8cf08e0465d70fec84
ronnywoods <rawoods93@gmail.com>
2019-03-04 19:37:14 -0800

    Adjust config to fix travis

--- database/indexNoSql.js
+++ database/indexNoSql.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 let databaseName = process.env.NODE_ENV === 'test' ? 'adidasnewtest' : 'adidasnewdev';
 const credentials = require('../config.js');
+const dbUser = process.env.NODE_ENV === 'test' ? credentials.testuser : credentials.user;
+const dbPw = process.env.NODE_ENV === 'test' ? credentials.testpw : credentials.pw;
 
 // using local database:
 // mongoose.connect(`mongodb://localhost/${databaseName}`, { useNewUrlParser: true });
 
 // using EC2 DB instance:
-mongoose.connect(`mongodb://${credentials.user}:${credentials.pw}@3.94.57.92/${databaseName}`, { useNewUrlParser: true });
+mongoose.connect(`mongodb://${dbUser}:${dbPw}@3.94.57.92/${databaseName}`, { useNewUrlParser: true });
  
 const db = mongoose.connection;
 

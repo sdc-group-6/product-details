6bdf11c935f4fd9f6a62e8dffc4b84672529bf8d
ronnywoods <rawoods93@gmail.com>
2019-03-11 11:54:31 -0700

    Adjust back to ec2 sharded database

--- database/indexNoSql.js
+++ database/indexNoSql.js
@@ -5,13 +5,14 @@ const dbUser = process.env.NODE_ENV === 'test' ? credentials.testuser : credenti
 const dbPw = process.env.NODE_ENV === 'test' ? credentials.testpw : credentials.pw;
 
 // using local database:
-mongoose.connect(`mongodb://localhost/${databaseName}`, { useNewUrlParser: true });
+// mongoose.connect(`mongodb://localhost/${databaseName}`, { useNewUrlParser: true });
 
 // using EC2 DB instance:
 // mongoose.connect(`mongodb://${dbUser}:${dbPw}@3.90.50.238/${databaseName}`, { useNewUrlParser: true });
 
 // using EC2 mongos instance:
-// mongoose.connect('mongodb://100.26.123.214/adidasnewdev2', { useNewUrlParser: true });
+// note: to run this database, go to app server in ec2 and enter: mongos --configdb config/3.88.151.0:27017,18.215.234.241:27017,54.92.177.245:27017 --bind_ip_all
+mongoose.connect('mongodb://100.26.123.214/adidasnewdev2', { useNewUrlParser: true });
  
 const db = mongoose.connection;
 

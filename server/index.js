f0af3b904906b43ce9f45120131d9696332ddd7f
ronnywoods <rawoods93@gmail.com>
2019-03-14 01:40:23 -0700

    Adjust newrelic back to require()

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-// require('newrelic');
+require('newrelic');
 import app from './nosqlServer';
 
 const PORT = process.env.NODE_ENV === 'test' ? process.env.PORT || 3000 : process.env.PORT || 8001;

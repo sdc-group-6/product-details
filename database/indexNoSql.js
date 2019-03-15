d5412077ef786f6dc6d1e15d9db08435b4a19695
ronnywoods <rawoods93@gmail.com>
2019-03-15 11:22:24 -0700

    Add forever to start script

--- database/indexNoSql.js
+++ database/indexNoSql.js
@@ -5,10 +5,10 @@ let databaseName = process.env.NODE_ENV === 'test' ? 'adidasnewtest' : 'adidasne
 // const dbPw = process.env.NODE_ENV === 'test' ? credentials.testpw : credentials.pw;
 
 // using local database:
-// mongoose.connect(`mongodb://localhost/${databaseName}`, { useNewUrlParser: true });
+mongoose.connect(`mongodb://localhost/${databaseName}`, { useNewUrlParser: true });
 
 // using EC2 DB instance:
-mongoose.connect(`mongodb://18.204.21.86:27017,54.174.171.238:27017,54.173.163.220:27017/${databaseName}?replicaSet=rs0`, { useNewUrlParser: true });
+// mongoose.connect(`mongodb://18.204.21.86:27017,54.174.171.238:27017,54.173.163.220:27017/${databaseName}?replicaSet=rs0`, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 
